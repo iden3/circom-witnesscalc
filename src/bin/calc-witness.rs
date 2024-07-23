f16159f20332fde6cdc9d569bb44ccbc436573fc
Oleg Lomaka <oleg.lomaka@gmail.com>
2024-07-23 11:36:14 -0400

    Move the code to calculate witness into library

--- src/bin/calc-witness.rs
+++ src/bin/calc-witness.rs
@@ -1,54 +1,9 @@
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
+use std::io::Write;
 use ruint::aliases::U256;
-use ruint::uint;
-use witness::{get_inputs_buffer, graph};
-use witness::graph::Node;
-use wtns_file::FieldElement;
-
-// TODO: use this constant from library
-pub const M: U256 =
-    uint!(21888242871839275222246405745257275088548364400416034343698204186575808495617_U256);
-
-pub fn get_inputs_size(nodes: &Vec<Node>) -> usize {
-    let mut start = false;
-    let mut max_index = 0usize;
-    for &node in nodes.iter() {
-        if let Node::Input(i) = node {
-            if i > max_index {
-                max_index = i;
-            }
-            start = true
-        } else if start {
-            break;
-        }
-    }
-    max_index + 1
-}
-
-pub fn populate_inputs(
-    input_list: &HashMap<String, Vec<U256>>,
-    inputs_info: &HashMap<String, (usize, usize)>,
-    input_buffer: &mut Vec<U256>,
-) {
-    for (key, value) in input_list {
-        let (offset, len) = inputs_info[key];
-        if len != value.len() {
-            panic!("Invalid input length for {}", key);
-        }
-        println!("input {}, offset {}, len {}", key, offset, len);
-
-        for (i, v) in value.iter().enumerate() {
-            input_buffer[offset + i] = v.clone();
-        }
-    }
-}
-
-fn conv(a: &U256) -> FieldElement<32> {
-    let x: [u8; 32] = a.as_le_slice().try_into().unwrap();
-    x.into()
-}
+use witness::{calc_witness, wtns_from_witness};
 
 struct Args {
     graph_file: String,
@@ -70,13 +25,7 @@ fn parse_args() -> Args {
     }
 }
 
-
-
 fn main() {
-    // let inputs_file = "/Users/alek/src/simple-circuit/circuit3_inputs.json";
-    // let graph_file = "/Users/alek/src/witness/graph_v2.bin";
-    // let witness_file = "/Users/alek/src/simple-circuit/output.wtns";
-
     let args = parse_args();
 
     let inputs_data = std::fs::read(&args.inputs_file).expect("Failed to read input file");
@@ -84,33 +33,13 @@ fn main() {
 
     let graph_data = std::fs::read(&args.graph_file).expect("Failed to read graph file");
 
-    let (nodes, signals, input_mapping): (Vec<Node>, Vec<usize>, HashMap<String, (usize, usize)>) =
-        postcard::from_bytes(graph_data.as_slice()).unwrap();
+    let witness = calc_witness(&inputs, &graph_data);
 
-    let inputs_size = get_inputs_size(&nodes);
-    println!("input size {}", inputs_size);
-    for &n in nodes.iter() {
-        match n {
-            Node::Input(s) => { println! {"input {}", s} },
-            _ => {}
-        }
-    }
-    let mut inputs_buffer = get_inputs_buffer(get_inputs_size(&nodes));
-    populate_inputs(&inputs, &input_mapping, &mut inputs_buffer);
-
-
-    println!("inputs: {:?}", inputs);
-
-    let witness = graph::evaluate(&nodes, inputs_buffer.as_slice(), &signals);
-    // println!("witness: {:?}", witness);
-
-    let vec_witness: Vec<FieldElement<32>> = witness.iter().map(|a| conv(a)).collect();
-    let mut wtns_f = wtns_file::WtnsFile::from_vec(vec_witness, conv(&M));
-    wtns_f.version = 2;
     {
-        let f = File::create(&args.witness_file).unwrap();
-        wtns_f.write(f).unwrap();
+        let mut f = File::create(&args.witness_file).unwrap();
+        let wtns_bytes = wtns_from_witness(witness);
+        f.write_all(&wtns_bytes).unwrap();
     }
-    println!("witness saved to {}", &args.witness_file);
 
+    println!("witness saved to {}", &args.witness_file);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,78 @@
 mod field;
 pub mod graph;
 
+use std::collections::HashMap;
 use ruint::aliases::U256;
+use crate::graph::Node;
+use wtns_file::FieldElement;
+use crate::field::M;
+
+// create a wtns file bytes from witness (array of field elements)
+pub fn wtns_from_witness(witness: Vec<U256>) -> Vec<u8> {
+    let vec_witness: Vec<FieldElement<32>> = witness.iter().map(|a| u256_to_field_element(a)).collect();
+    let mut buf = Vec::new();
+    let mut wtns_f = wtns_file::WtnsFile::from_vec(vec_witness, u256_to_field_element(&M));
+    wtns_f.version = 2;
+    // We write into the buffer, so we should not have any errors here.
+    // Panic in case of out of memory is fine.
+    wtns_f.write(&mut buf).unwrap();
+    buf
+}
+
+pub fn calc_witness(
+    inputs: &HashMap<String, Vec<U256>>,
+    graph_data: &[u8]) -> Vec<U256> {
+
+    let (nodes, signals, input_mapping): (Vec<Node>, Vec<usize>, HashMap<String, (usize, usize)>) =
+        postcard::from_bytes(graph_data).unwrap();
+
+    let mut inputs_buffer = get_inputs_buffer(get_inputs_size(&nodes));
+    populate_inputs(inputs, &input_mapping, &mut inputs_buffer);
+
+    graph::evaluate(&nodes, inputs_buffer.as_slice(), &signals)
+}
+
+fn get_inputs_size(nodes: &Vec<Node>) -> usize {
+    let mut start = false;
+    let mut max_index = 0usize;
+    for &node in nodes.iter() {
+        if let Node::Input(i) = node {
+            if i > max_index {
+                max_index = i;
+            }
+            start = true
+        } else if start {
+            break;
+        }
+    }
+    max_index + 1
+}
+
+fn populate_inputs(
+    input_list: &HashMap<String, Vec<U256>>,
+    inputs_info: &HashMap<String, (usize, usize)>,
+    input_buffer: &mut Vec<U256>) {
+    for (key, value) in input_list {
+        let (offset, len) = inputs_info[key];
+        if len != value.len() {
+            panic!("Invalid input length for {}", key);
+        }
+        println!("input {}, offset {}, len {}", key, offset, len);
+
+        for (i, v) in value.iter().enumerate() {
+            input_buffer[offset + i] = v.clone();
+        }
+    }
+}
+
+fn u256_to_field_element(a: &U256) -> FieldElement<32> {
+    let x: [u8; 32] = a.as_le_slice().try_into().unwrap();
+    x.into()
+}
 
 
 /// Allocates inputs vec with position 0 set to 1
-pub fn get_inputs_buffer(size: usize) -> Vec<U256> {
+fn get_inputs_buffer(size: usize) -> Vec<U256> {
     let mut inputs = vec![U256::ZERO; size];
     inputs[0] = U256::from(1);
     inputs
@@ -16,10 +83,6 @@ pub fn get_inputs_buffer(size: usize) -> Vec<U256> {
 mod tests {
     #[test]
     fn test_ok() {
-        // build_witness();
-        for i in 0..3 {
-            println!("{}", i);
-        }
         println!("OK");
     }
 
